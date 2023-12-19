02f23a62764d0fe673dcd026ad8115c4c74d5afb
Roxana <roxanna.sa@gmail.com>
2023-12-18 23:54:13 -0300

    Header styles and popover menu

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,4 +1,5 @@
 import { Button } from './Button';
+import PopOver from './PopOver';
 import './header.css';
 
 type User = {
@@ -12,10 +13,118 @@ interface HeaderProps {
   onCreateAccount: () => void;
 }
 
+
+// #region Íconos
+
+const IconOne = () => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <path
+        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+        stroke="#FB923C"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+    </svg>
+  )
+}
+
+const IconTwo = () => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <path
+        d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
+        stroke="#FB923C"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+    </svg>
+  )
+}
+
+const IconThree = () => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
+      <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
+      <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
+      <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
+      <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
+      <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
+    </svg>
+  )
+}
+
+//#endregion
+
+// Header menu options
+const options = [
+  {
+    name: 'Booklist',
+    description: 'See a list of all the available books',
+    href: '/',
+    icon: IconOne,
+  },
+  {
+    name: 'Favourites',
+    description: 'See a list of your favourites!',
+    href: '/favourites',
+    icon: IconTwo,
+  },
+  {
+    name: 'Add new book',
+    description: 'Add a new book to the collection',
+    href: '/newBook',
+    icon: IconThree,
+  },
+]
+
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
   <header>
     <div className="storybook-header">
       <div>
+
+        <PopOver options={options}></PopOver>
 
         <svg className="svg-icon" viewBox="0 0 20 20">
 					<circle fill="none" cx="7.884" cy="11.397" r="0.699"></circle>
@@ -24,6 +133,8 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
 				</svg>
 
         <h1>A Song of Ice and Fire | Reading book club</h1>
+
+        
       </div>
       <div>
         {user ? (
@@ -31,12 +142,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <Button customClassName="bg-teal-600" size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button customClassName="bg-gray-600" size="small" onClick={onLogin} label="Log in" />
+            <Button customClassName="bg-gray-600" primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>
