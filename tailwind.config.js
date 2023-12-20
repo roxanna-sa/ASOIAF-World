e4aab6fd0e714c5e944116e23e6cbbf82d99c227
Roxana <roxanna.sa@gmail.com>
2023-12-19 21:27:25 -0300

    Add jest/testing-library & config. tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        righteous: ['Righteous', 'cursive'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
