93f7871bcffa9b37b43b6199a9d01cb98553848c
Roxana <roxanna.sa@gmail.com>
2023-12-19 23:09:37 -0300

    Component stories

--- src/stories/BookDialog.tsx
+++ src/stories/BookDialog.tsx
@@ -50,7 +50,7 @@ export const BookDialog = ({ isOpen, setIsOpen, modalTitle, closeButtonText, chi
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-medium  text-center leading-6 text-gray-900"
                   >
                     { modalTitle ?? "Modal" }
                   </Dialog.Title>
